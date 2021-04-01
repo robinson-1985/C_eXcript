62e2e9942bf15fd06163f1f3bf3abadfc2c44cea
Robinson <robinpaulista85@gmail.com>
2021-04-01 18:40:08 -0300

    Aprendendo a linguagem C

---
+++ programando_em_C/programa21_modificadores_de_tipo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int i = 10;
+
+    short int u;
+    printf("%i -> variavel i\n", sizeof(i));
+    printf("%i -> variavel u\n", sizeof(u));
+
+    /* 
+    
+    signed = + e -;
+    unsigned = +;
+    long = aumentar a capacidade de armazenamento da nossa variável;
+    short = quando queremos diminuir o  tamanho de armazenamento.
+
+    */
+
+
+    return 0;
+}
